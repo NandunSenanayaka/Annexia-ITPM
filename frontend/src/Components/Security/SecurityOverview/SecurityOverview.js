dafae3501178b57a4a9000a826417c37f2643245
Nandun Senanayaka <nandunsenanayaka21@gmail.com>
2025-02-26 22:38:11 +0530

    fixed some error in file

--- frontend/src/Components/Security/SecurityOverview/SecurityOverview.js
+++ frontend/src/Components/Security/SecurityOverview/SecurityOverview.js
@@ -2,48 +2,18 @@ import React, { useEffect, useState } from 'react';
 import './SecurityOverview.css';
 import SecurityDashboard from '../SecurityDashboard/SecurityDashboard';
 import axios from 'axios';
-import SecurityOverviews from './SecurityOverview';  // Correct import
-import AddSecurity from '../SecurityDetails/SecurityDetails';  // Correct import
+import { Link } from 'react-router-dom';  // Ensure Link is imported
+import { FaHome, FaPlus, FaFileAlt } from 'react-icons/fa';  // Assuming these are imported
 
 
 function SecurityOverview() {
-  return (                                                         
-    <div className="container">
-      <aside className="sidebar">
-        <h2 className="logo">ANNEXIA</h2>
-        <nav>
-          <ul>
-            <Link to="/addsecurity"style={{ textDecoration: "none", color: "inherit" }}>
-              <li><FaHome /> Overview</li>
-            </Link>
-
-            <Link
-              to="/addnotice"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <li>
-                <FaPlus /> Add Notice
-              </li>
-            </Link>
-
-            <Link
-              to="/securitynotices"
-              style={{ textDecoration: "none", color: "inherit" }}
-            >
-              <li>
-                <FaFileAlt /> Security Notices
-              </li>
-            </Link>
-
-const URL = 'http://localhost:5000/security';
-
-
-const fetchHandler = async () => {
-  return await axios.get(URL).then((res) => res.data);
-};
-
-const SecurityOverview = () => {
   const [security, setSecurity] = useState([]);  // State to hold security data
+
+  const URL = 'http://localhost:5000/security';
+
+  const fetchHandler = async () => {
+    return await axios.get(URL).then((res) => res.data);
+  };
 
   useEffect(() => {
     fetchHandler().then((data) => {
@@ -53,22 +23,42 @@ const SecurityOverview = () => {
   }, []);
 
   return (
-    <div className="securitydashboard-container">
-      <SecurityDashboard />
+    <div className="container">
+      <aside className="sidebar">
+        <h2 className="logo">ANNEXIA</h2>
+        <nav>
+          <ul>
+            <Link to="/addsecurity" style={{ textDecoration: "none", color: "inherit" }}>
+              <li><FaHome /> Overview</li>
+            </Link>
 
-      <div className="securitydashboard-content">
-        <h1>Overview</h1>
-        <div>
-          {security && security.map((securityItem, i) => (
-            <div key={i}>
-              {/* Passing securityItem to AddSecurity */}
-              <AddSecurity {...securityItem} />
-            </div>
-          ))}
+            <Link to="/addnotice" style={{ textDecoration: "none", color: "inherit" }}>
+              <li><FaPlus /> Add Notice</li>
+            </Link>
+
+            <Link to="/securitynotices" style={{ textDecoration: "none", color: "inherit" }}>
+              <li><FaFileAlt /> Security Notices</li>
+            </Link>
+          </ul>
+        </nav>
+      </aside>
+
+      <div className="securitydashboard-container">
+        <SecurityDashboard />
+        <div className="securitydashboard-content">
+          <h1>Overview</h1>
+          <div>
+            {security && security.map((securityItem, i) => (
+              <div key={i}>
+                {/* Passing securityItem to AddSecurity */}
+                <AddSecurity {...securityItem} />
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default SecurityOverview;
