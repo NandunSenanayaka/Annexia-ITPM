75d530247d44d1837175fe93f7b185c391288901
Sahan Udara <76866708+Sahan-Udara@users.noreply.github.com>
2025-02-26 20:53:00 +0530

    HomePage Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,12 +3,13 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Im
 import './App.css';
 import SecurityOverview from './Components/Security/SecurityOverview/SecurityOverview';
 import AddSecurity from "./Components/Security/AddSecurity/AddSecurity";
+import HomePage from './Components/Pages/HomePage/HomePage';
 function App() {
   return (
     <div >
       <React.Fragment>
         <Routes>
-          <Route path="/" element={<SecurityOverview />} />
+          <Route path="/" element={<HomePage />} />
           <Route path="/addsecurity" element={<AddSecurity />} />
         </Routes>
     </React.Fragment>
